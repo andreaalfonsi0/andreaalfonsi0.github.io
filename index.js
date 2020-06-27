0513fceade2f992e019b28e1c8a7b4f009a047e4
andrea <andrea.alfonsi2000@gmail.com>
2020-06-27 17:43:25 +0200

    First working solution

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function drawPaperTrajector() {
   path.id = DASHED_PATH_ID;
   path.setAttribute("stroke-width", "4");
   path.setAttribute("d", createPlanePath().path);
-  path.setAttribute("stroke", "#ddd");
+  path.setAttribute("stroke", "var(--paper-trajectory-color)");
   path.setAttribute("fill", "none");
   path.setAttribute("stroke-linecap", "round")
   svg.appendChild(path);
@@ -29,10 +29,44 @@ function drawPaperTrajector() {
   path = path.cloneNode();
   path.id = PAPER_PATH_ID;
   path.setAttribute("stroke-width", "5")
-  path.setAttribute("stroke", "white");
-  path.style.strokeDasharray = path.getTotalLength();
+  path.setAttribute("stroke", "var(--background-color)");
+  let length = path.getTotalLength();
+  path.setAttribute("style", `stroke-dashoffset:${Math.round(length * getScrollPercentage(document.body) / 100)};stroke-dasharray:${Math.max(100, length)}`)
   svg.appendChild(path);
 
+}
+
+
+function createPlanePath() {
+  let plane = document.querySelector("#plane");
+  let paragraphs = Array.apply(null, document.querySelectorAll(".section-title")).reverse();
+  let contactmeButton = document.querySelector("#contactMe");
+  let title = document.querySelector('.title');
+  let svgBuilder = new SVGBuilder();
+  let planeRect = plane.getBoundingClientRect();
+  let contactmeButtonRect = contactmeButton.getBoundingClientRect();
+  svgBuilder.moveTo([contactmeButtonRect.x + window.pageXOffset + contactmeButtonRect.width / 2, contactmeButtonRect.top + window.pageYOffset + contactmeButtonRect.height / 2]);
+  let center;
+  for (let i = 0; i < paragraphs.length; i++) {
+    center = findCenter(paragraphs[i]);
+    svgBuilder.lineTo(center)
+  }
+  svgBuilder.lineTo(findCenter(title))
+  svgBuilder.lineTo([planeRect.x + window.pageXOffset - 10, planeRect.top + window.pageYOffset - 10])
+  return svgBuilder;
+}
+
+function getScrollPercentage() {
+  var h = document.documentElement,
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+  return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+}
+
+function findCenter(element){
+  let elRect = element.getBoundingClientRect();
+  return [elRect.x + window.pageXOffset + elRect.width/2, elRect.top + window.pageYOffset + elRect.height/2];
 }
 
 window.oncontextmenu = function() {
@@ -68,44 +102,13 @@ window.onscroll = function() {
     let perc = getScrollPercentage(document.body);
     let path = document.querySelector(`#${PAPER_PATH_ID}`)
     let length = path.getTotalLength();
-    // path.style.strokeDashoffset = Math.round(- length * perc);
+    path.setAttribute("style", `stroke-dashoffset:${length * perc / 100 };stroke-dasharray:${length}`);
+    // path.style.strokeDashoffset = "" + Math.round(length * perc / 100);
   }, 100);
 }
 
 window.onresize = function() {
-  let path = createPlanePath().path;
-  document.querySelector(`#${PAPER_PATH_ID}`).setAttribute("d", path)
-  document.querySelector(`#${DASHED_PATH_ID}`).setAttribute("d", path)
-}
 
-function createPlanePath() {
-  let plane = document.querySelector("#plane");
-  let paragraphs = Array.apply(null, document.querySelectorAll(".section-title")).reverse();
-  let contactmeButton = document.querySelector("#contactMe");
-  let title = document.querySelector('.title');
-  let svgBuilder = new SVGBuilder();
-  let planeRect = plane.getBoundingClientRect();
-  let contactmeButtonRect = contactmeButton.getBoundingClientRect();
-  svgBuilder.moveTo([contactmeButtonRect.x + window.pageXOffset + contactmeButtonRect.width / 2, contactmeButtonRect.top + window.pageYOffset + contactmeButtonRect.height / 2]);
-  let center;
-  for (let i = 0; i < paragraphs.length; i++) {
-    center = findCenter(paragraphs[i]);
-    svgBuilder.lineTo(center)
-  }
-  svgBuilder.lineTo(findCenter(title))
-  svgBuilder.lineTo([planeRect.x + window.pageXOffset - 10, planeRect.top + window.pageYOffset - 10])
-  return svgBuilder;
-}
-
-function getScrollPercentage() {
-  var h = document.documentElement,
-    b = document.body,
-    st = 'scrollTop',
-    sh = 'scrollHeight';
-  return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-}
-
-function findCenter(element){
-  let elRect = element.getBoundingClientRect();
-  return [elRect.x + window.pageXOffset + elRect.width/2, elRect.top + window.pageYOffset + elRect.height/2];
+  document.querySelector('svg').remove();
+  drawPaperTrajector();
 }
